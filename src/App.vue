b989af371d5449524067b47c0a7f464e46792835
takashimakyohei <hagi.suzu@icloud.com>
2020-06-17 18:36:02 +0900

    レイアウト作成

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,37 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <header>ヘッダー</header>
+    <div class="container">
+      <main>収支</main>
+      <div class="column">
+        <aside>収入</aside>
+        <aside>支出</aside>
+      </div>
     </div>
-    <router-view/>
   </div>
 </template>
 
+<script>
+export default {};
+</script>
+
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  width: 1000px;
+  margin: 0 auto;
 }
-
-#nav {
-  padding: 30px;
+.container {
+  display: flex;
 }
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.column {
+  flex-direction: column;
 }
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+main {
+  flex: 1;
+  height: 1000px;
+}
+aside {
+  width: 450px;
+  height: 500px;
 }
 </style>
